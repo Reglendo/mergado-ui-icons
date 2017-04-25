8116ff257486c6bebd4d1f54d067e03d3005cb97
Martin Hrncir <martin.hrncir@reglendo.com>
2017-04-25 11:43:41 +0200

    flip icons

---
+++ export/icons/flip-vertical.tsx
@@ -0,0 +1,4 @@
+
+import * as React from "react"
+export const IconFlipVertical: JSX.Element = 
+        <g><g id="g3394" transform="translate(-0.84745756,-0.62109513)">    <path  id="path3357-6" d="m 33.38156,3.7812812 0,15.2196908 -24.9982253,0 z" style={{"fill":"#000000","fillRule":"evenodd","stroke":"#000000","strokeWidth":1.37589288,"strokeLinecap":"butt","strokeLinejoin":"miter","strokeOpacity":1}}></path>    <path  id="path3357-3-7" d="m 33.381559,37.460909 0,-15.219691 -24.9982235,0 z" style={{"fill":"#ffffff","fillRule":"evenodd","stroke":"#000000","strokeWidth":1.37589288,"strokeLinecap":"butt","strokeLinejoin":"miter","strokeOpacity":1}}></path>  </g></g>;
