0518a46a3cd610e1a1fa927f6fbf1f070db7b835
Unknown <martin.hrncir@reglendo.com>
2017-09-23 15:13:15 +0200

    hint icons

---
+++ icons/IconHintHelp.tsx
@@ -0,0 +1,52 @@
+
+
+import * as React from "react"
+
+export interface Props {
+    size?: number
+    text?: string
+    title?: string
+    style?: any
+    addClass?: string
+    viewBox?: string
+}
+export interface State {
+}
+
+class IconHintHelp extends React.Component<Props, State> {
+
+    readonly name = "muk-icon";
+
+    public static defaultProps: Props = {
+        size: 15,
+        style: {},
+        addClass: "",
+        title: "",
+        viewBox: "0 0 10.583333 10.583333",
+    }
+
+    render() {
+        let className = `${this.name} ${this.name}--hint-help ${this.props.addClass}`
+
+        return (
+            <span className={className} style={this.props.style} title={this.props.title}>
+                <svg className={`${this.name}__image`} preserveAspectRatio='xMidYMid meet'
+                     fill='currentColor'
+                     height={this.props.size}
+                     width={this.props.size}
+                     viewBox={this.props.viewBox}
+                >
+                    <g><g   id="layer1" transform="translate(0,-286.41667)">    <g id="g151" transform="matrix(0.02081203,0,0,0.02081203,0,286.41667)">      <g id="g93">        <g id="g91">          <g id="g89">            <path id="path87" d="M 254.26,0 C 113.845,0 0,113.845 0,254.26 0,394.675 113.845,508.52 254.26,508.52 394.675,508.52 508.52,394.675 508.52,254.26 508.52,113.845 394.675,0 254.26,0 Z m 0,446.353 c -18.084,0 -32.736,-14.652 -32.736,-32.736 0,-18.052 14.652,-32.704 32.736,-32.704 18.052,0 32.704,14.652 32.704,32.704 0,18.084 -14.652,32.736 -32.704,32.736 z m 57.717,-156.306 c -13.349,7.342 -25.935,21.866 -25.935,27.778 0,17.544 -14.239,31.783 -31.782,31.783 -17.543,0 -31.782,-14.239 -31.782,-31.783 0,-40.364 36.995,-71.447 58.861,-83.461 32.164,-17.735 36.486,-37.058 36.486,-53.585 0,-46.688 -39.823,-53.649 -63.565,-53.649 -30.607,0 -63.565,20.087 -63.565,64.232 0,17.544 -14.239,31.782 -31.782,31.782 -17.543,0 -31.782,-14.239 -31.782,-31.782 0,-83.016 65.504,-127.797 127.13,-127.797 63.279,0 127.13,36.232 127.13,117.214 -0.001,47.133 -23.33,83.937 -69.414,109.268 z" style={{"fill":"#010002"}} ></path>          </g>        </g>      </g>      <g id="g95">      <g id="g97">      <g id="g99">      <g id="g101">      <g id="g103">      <g id="g105">      <g id="g107">      <g id="g109">      <g id="g111">      <g id="g113">      <g id="g115">      <g id="g117">      <g id="g119">      <g id="g121">      <g id="g123">    </g>  </g></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g>
+                </svg>
+                {this.props.text ? (
+                    <span className={`${this.name}__text`}>
+                        {this.props.text}
+                    </span>
+                ) : null}
+            </span>
+        )
+    }
+}
+
+export default IconHintHelp
+
