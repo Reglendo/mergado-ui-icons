a4459a250f8575311e5148c763a119f10a41709a
Martin Hrnčíř <martin.hrncir@reglendo.com>
2018-08-17 10:15:04 +0200

    added zbozi.cz builded icon

---
+++ lib/icons/IconZboziCz.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="react" />
+import * as React from "react";
+export interface Props {
+    size?: number;
+    text?: string | Element | JSX.Element;
+    title?: string;
+    style?: any;
+    addClass?: string;
+    viewBox?: string;
+    color?: string;
+    textFirst?: boolean;
+    className?: string;
+}
+declare class IconZboziCz extends React.PureComponent<Props, {}> {
+    static defaultProps: Props;
+    render(): JSX.Element;
+}
+export default IconZboziCz;
